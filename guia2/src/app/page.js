a498172305ddc300b1a6c29a0d5beb3bd958efce
AlexHernandez2698632494 <piojosevaldi@gmail.com>
2024-07-05 17:39:55 -0600

    ejemplo1 part 2

--- guia2/src/app/page.js
+++ guia2/src/app/page.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-const element = <h1>Hola, Mundo!</h1>;
-
+const element = (
+  <><h1>Hola, Mundo!</h1>
+  <h2>Son las {new Date().toTimeString() }</h2> </>
+  )
 export default function Home() {
   return (
     <main className={styles.main}>
